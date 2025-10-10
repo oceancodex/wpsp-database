bd6cd596b2669f6977dbaa1395e0ce82dc23b66d
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-10 19:27:58 +0700

    KDN-PC-01: Update

---
+++ src/Traits/ModelsTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WPSPCORE\Database\Traits;
+
+use WPSPCORE\Funcs;
+
+trait ModelsTrait {
+
+	public $roleModel;
+	public $permissionModel;
+	public $funcs;
+
+	public function __construct($attributes = []) {
+		$this->funcs           = Funcs::getInstance();
+		$this->roleModel       = Funcs::getInstance()->_config('permission.models.role');
+		$this->permissionModel = Funcs::getInstance()->_config('permission.models.permission');
+		$this->connection      = Funcs::getInstance()->_getAppShortName() . '_' . $this->connection;
+		$this->customPrefix();
+		parent::__construct($attributes);
+	}
+
+	protected function customPrefix(): void {
+		if (!empty($this->prefix)) {
+			$this->getConnection()->setTablePrefix($this->prefix);
+		}
+	}
+
+}
