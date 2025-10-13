2632cac2449da6b10548b0fdb522ccef0858c870
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-13 10:17:24 +0700

    KDN-PC-01: Update

--- src/Base/BaseMongoDBModel.php
+++ src/Base/BaseMongoDBModel.php
@@ -2,7 +2,39 @@
 namespace WPSPCORE\Database\Base;
 
 use MongoDB\Laravel\Eloquent\Model;
+use WPSPCORE\Traits\BaseInstancesTrait;
 
 class BaseMongoDBModel extends Model {
+
+	use BaseInstancesTrait;
+
+	public function __construct($attributes = []) {
+		$this->beforeBaseInstanceConstruct();
+		$this->connection = $this->funcs->_getAppShortName() . '_' . $this->connection;
+		$this->customPrefix();
+		parent::__construct($attributes);
+	}
+
+	/*
+	 *
+	 */
+
+	protected function customPrefix() {
+		if (!empty($this->prefix)) {
+			$this->getConnection()->setTablePrefix($this->prefix);
+		}
+	}
+
+	/*
+	 *
+	 */
+
+	protected function beforeConstruct() {}
+
+	protected function beforeInstanceConstruct() {}
+
+	protected function afterConstruct() {}
+
+	protected function afterInstanceConstruct() {}
 
 }
